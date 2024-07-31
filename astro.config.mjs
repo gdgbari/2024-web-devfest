5dca1254a848e53f150811c033048e9b4d6211a7
Simone Bonfrate <info@bonfry.com>
2024-07-31 20:33:47 +0200

    added faq

--- astro.config.mjs
+++ astro.config.mjs
@@ -69,7 +69,7 @@ export default defineConfig({
     defaultLocale: "it",
     locales: [ "en", "it"],
     routing: {
-        prefixDefaultLocale: true
+        prefixDefaultLocale: false,
     }
   }
 });
